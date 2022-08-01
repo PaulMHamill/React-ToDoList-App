7335ea1f609d9871a6abedc89daf7436d6105d39
Paul Hamill <enquiries@nestboxproperty.co.uk>
2022-08-01 14:23:28 +0100

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([
+    { name: "Buy Shopping"},
+    { name: "Clean Bathroom"},
+    { name: "Car's MOT"},
+  ]);
+
+  const [newItem, setNewItem] = useState("");
+
+  const itemNodes = items.map((item, index) => {
+    return(
+        <li key={index}><span>{item.name}</span></li>
+    )
+});
+
+  const handleItemInput = (event) => {
+    setNewItem(event.target.value);
+  };
+
+  const saveNewItem = (event) => {
+    event.preventDefualt();
+    const copyItems = [...items]
+    copyItems.push({name: newItem})
+    setItems(copyItems)
+    setNewItem("")
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List</h1>
+      <hr></hr>
+
+      <ul>
+        {itemNodes}
+      </ul>
+
+      <form onSubmit={saveNewItem}>
+        <label htmlFor="new-item">Add a new item:</label>  
+        <input id="new-item" type="text" value={newItem} onChange={handleItemInput}/>           
+        <input type="submit" value="Save New Item" />
+      </form>
+
     </div>
   );
 }
